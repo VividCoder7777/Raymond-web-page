f4020cce6d767fe260aa82a1aed6289b1c1e1e69
Raymond Yu <raymond.yu171@gmail.com>
2019-06-29 16:12:22 -0400

    added some pictures

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  logos = [];
+  baseImagePath = "../../../assets/images/";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.logos.push(`${this.baseImagePath}/java_logo.jpg`);
+    this.logos.push(`${this.baseImagePath}/C_logo.png`);
+    this.logos.push(`${this.baseImagePath}/asp.net.jpg`);
+    this.logos.push(`${this.baseImagePath}/spring_logo.png`);
+    this.logos.push(`${this.baseImagePath}/angular_logo.png`);
+    this.logos.push(`${this.baseImagePath}/react_logo.jpg`);
+    this.logos.push(`${this.baseImagePath}/node_logo.png`);
   }
-
 }
